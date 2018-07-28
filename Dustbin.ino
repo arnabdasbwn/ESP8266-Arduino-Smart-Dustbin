a4a436ada65c943517357e0a874dd6e449cb06d3
Arnab Kumar Das Crazy Engineer <arnab.das.bwn@gmail.com>
2018-07-28 18:49:59 +0530

    Source Code
    Visit http://www.arnabkumardas.com/blog/diy-project/iot-smart-dustbin/ to learn how to use this.

---
+++ Dustbin.ino
@@ -0,0 +1,120 @@
+#include <NewPing.h>
+
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 50 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+//*-- IoT Information
+#define SSID "CrazyEngineer"
+#define PASS "www.ArnabKumarDas.com"
+#define IP "184.106.153.149" // ThingSpeak IP Address: 184.106.153.149
+
+// GET /update?key=[THINGSPEAK_KEY]&field1=[data 1]&field2=[data 2]...;
+String GET = "GET /update?key=VYJGZSHIYR7SMTX9"; // You have to get your key from the website 
+int prev = 0;
+void setup() {
+  Serial.begin( _baudrate );
+  
+  sendDebug("AT");
+  delay(5000);
+
+  if (Serial.find("OK"))
+  {
+    debug.println("RECEIVED: OK\nData ready to sent!");
+    connectWiFi();
+  }
+
+}
+
+void loop()
+{
+  int dat = sonar.ping_cm();
+  if (dat < 0 || dat > 37)
+    dat = prev;
+  String depth = String((37 - dat)); // turn integer to string
+  debug.print( "DEPTH = " );
+  debug.println(depth);
+  updateTS(depth);
+  delay(1000);
+}
+//----- update the  Thingspeak string with 3 values
+void updateTS( String T)
+{
+  // ESP8266 Client
+  String cmd = "AT+CIPSTART=\"TCP\",\"";// Setup TCP connection
+  cmd += IP;
+  cmd += "\",80";
+  sendDebug(cmd);
+  delay(2000);
+  if ( Serial.find( "Error" ) )
+  {
+    debug.print( "RECEIVED: Error\nExit1" );
+    return;
+  }
+
+  cmd = GET + "&field1=" + T + "\r\n";
+  Serial.print( "AT+CIPSEND=" );
+  Serial.println( cmd.length() );
+  if (Serial.find( ">" ) )
+  {
+    debug.print(">");
+    debug.print(cmd);
+    Serial.print(cmd);
+  }
+  else
+  {
+    sendDebug( "AT+CIPCLOSE" );//close TCP connection
+  }
+  if ( Serial.find("OK") )
+  {
+    debug.println( "RECEIVED: OK" );
+  }
+  else
+  {
+    debug.println( "RECEIVED: Error\nExit2" );
+  }
+}
+
+void sendDebug(String cmd)
+{
+  debug.print("SEND: ");
+  debug.println(cmd);
+  Serial.println(cmd);
+}
+
+boolean connectWiFi()
+{
+  Serial.println("AT+CWMODE=1");//WiFi STA mode - if '3' it is both client and AP
+  delay(2000);
+  //Connect to Router with AT+CWJAP="SSID","Password";
+  // Check if connected with AT+CWJAP?
+  String cmd = "AT+CWJAP=\""; // Join accespoint
+  cmd += SSID;
+  cmd += "\",\"";
+  cmd += PASS;
+  cmd += "\"";
+  sendDebug(cmd);
+  delay(5000);
+  if (Serial.find("OK"))
+  {
+    debug.println("RECEIVED: OK");
+    return true;
+  }
+  else
+  {
+    debug.println("RECEIVED: Error");
+    return false;
+  }
+
+  cmd = "AT+CIPMUX=0";// Set Single connection
+  sendDebug( cmd );
+  if ( Serial.find( "Error") )
+  {
+    debug.print( "RECEIVED: Error" );
+    return false;
+  }
+}
+
+
